b81bf020c775662cb2a333da13d4293eb0fa92fc
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2018-10-18 23:12:00 -0300

    New Release

---
+++ Parte Gabriel/ProjetoEvers/src/ConexaoBD/daoEvers.java
@@ -0,0 +1,60 @@
+package ConexaoBD;
+
+import Modelo.Modelo;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class daoEvers {
+    
+    Conexao con = new Conexao();
+    Modelo mod = new Modelo();
+    
+    
+       public void Salvar(Modelo med){
+        con.conexao();
+        
+        try {
+            PreparedStatement pst = con.con.prepareStatement("insert into produtos (nome,lote) values (?,?)");
+            pst.setString(1,med.getNome());
+            pst.setDouble(2,med.getLote());
+            pst.execute();
+            
+            JOptionPane.showMessageDialog(null,"Dados inseridos com sucesso!!","A V I S O",1);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro: \n"+ex);
+        }
+        
+        con.desconecta();
+    }
+       public void Excluir(Modelo mod){
+    
+        con.conexao();
+    
+        try {
+            PreparedStatement pst = con.con.prepareStatement("delete from produtos where lote= ?");
+            pst.setDouble(2,mod.getLote());
+            pst.execute();
+            JOptionPane.showMessageDialog(null,"Dados excluidos com sucesso","Parabéns",2);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao excluir dados","CAUTION",3);
+        }
+        
+        con.desconecta();
+    }
+    
+    public Modelo buscaProdutos (Modelo med){
+        con.conexao();
+        con.executaSQL("select * from produtos where nome like '%"+med.getPesquisa()+"%'");
+        try {
+            con.rs.first();
+            med.setNome(con.rs.getString("nome"));
+            med.setLote(con.rs.getDouble("lote"));
+                    } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erroao buscar medico: \n"+ex);
+        }
+        con.desconecta();
+        return med;
+    }
+}
---
+++ Parte Gabriel/ProjetoEvers/src/Modelo/Modelo.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+public class Modelo {
+    
+    private String nome;
+    private Double lote;
+    private String pesquisa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getLote() {
+        return lote;
+    }
+
+    public void setLote(Double lote) {
+        this.lote = lote;
+    }
+       public String getPesquisa() {
+        return pesquisa;
+    }
+
+    public void setPesquisa(String pesquisa) {
+        this.pesquisa = pesquisa;
+    }
+}
